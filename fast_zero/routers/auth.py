7165b697c57c61c0f291244ee4777ee705e63a69
LuanSoaresMoreira <luan.lv1@hotmail.com>
2025-02-28 09:39:11 -0300

    ⭐Implementação da rota refresh token

--- fast_zero/routers/auth.py
+++ fast_zero/routers/auth.py
@@ -8,7 +8,11 @@ from sqlalchemy.orm import Session
 from fast_zero.database import get_session
 from fast_zero.models import User
 from fast_zero.schemas import Token
-from fast_zero.security import create_access_token, verify_password
+from fast_zero.security import (
+    create_access_token,
+    get_current_user,
+    verify_password,
+)
 
 router = APIRouter(prefix='/auth', tags=['auth'])
 
@@ -19,10 +23,21 @@ T_OAuth2Form = Annotated[OAuth2PasswordRequestForm, Depends()]
 @router.post('/token', response_model=Token)
 def login_for_access_token(session: T_Session, form_data: T_OAuth2Form):
     user = session.scalar(select(User).where(User.email == form_data.username))
-    if not user or not verify_password(form_data.password, user.password):
+
+    if user is None or not verify_password(form_data.password, user.password):
         raise HTTPException(
             status_code=400, detail='Incorrect email or password'
         )
+
     access_token = create_access_token(data_claims={'sub': user.email})
 
     return {'access_token': access_token, 'token_type': 'Bearer'}
+
+
+@router.post('/refresh_token', response_model=Token)
+def refresh_access_token(
+    user: User = Depends(get_current_user),
+):
+    new_access_token = create_access_token(data_claims={'sub': user.email})
+
+    return {'access_token': new_access_token, 'token_type': 'bearer'}
